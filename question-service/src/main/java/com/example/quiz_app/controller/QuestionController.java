7a18b24b23e180feadc1e7b94e24c26c582fc9fa
UVM <utkarshmore7588@gmail.com>
2025-06-16 11:57:38 +0530

    first commit

---
+++ question-service/src/main/java/com/example/quiz_app/controller/QuestionController.java
@@ -0,0 +1,40 @@
+package com.example.quiz_app.controller;
+
+import com.example.quiz_app.model.Question;
+import com.example.quiz_app.service.QuestionService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("question")
+public class QuestionController {
+
+    QuestionService questionService;
+    public QuestionController (QuestionService questionService){
+        this.questionService=questionService;
+    }
+    @RequestMapping("allQuestions")
+    public List<Question> getQuestions(){
+        return questionService.getQuestions();
+    }
+
+    @GetMapping("category/{category}")
+    public ResponseEntity<List<Question>> getQuestionsByCategory(@PathVariable String category){
+        return questionService.getQuestionsByCategory(category);
+    }
+
+    @PostMapping("add")
+    public ResponseEntity<String> addQuestion(@RequestBody Question question){
+        return  questionService.addQuestion(question);
+    }
+
+    @GetMapping("generate")
+    public ResponseEntity<List<Integer>> getQuestionsForQuiz
+            (@RequestParam String categoryName, @RequestParam Integer numQuestions ){
+        return questionService.getQuestionsForQuiz(categoryName, numQuestions);
+    }
+
+
+}
---
+++ question-service/src/main/java/com/example/quiz_app/model/Question.java
@@ -0,0 +1,21 @@
+package com.example.quiz_app.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "questions")
+@Data
+public class Question  {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Integer id;
+    private  String category;
+    private  String difficultyLevel;
+    private  String questionTitle;
+    private  String option1;
+    private  String option2;
+    private  String option3;
+    private  String option4;
+    private  String rightAnswer;
+}
