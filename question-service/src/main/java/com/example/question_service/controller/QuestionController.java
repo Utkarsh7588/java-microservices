b4ccb76125662b4e29e4eb3b4af897ee29a52db3
UVM <utkarshmore7588@gmail.com>
2025-06-18 18:33:32 +0530

    api getway added

--- question-service/src/main/java/com/example/question_service/controller/QuestionController.java
+++ question-service/src/main/java/com/example/question_service/controller/QuestionController.java
@@ -4,6 +4,7 @@ import com.example.question_service.model.Question;
 import com.example.question_service.model.QuestionWrapper;
 import com.example.question_service.model.Response;
 import com.example.question_service.service.QuestionService;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,38 +14,44 @@ import java.util.List;
 @RequestMapping("question")
 public class QuestionController {
 
+    Environment environment;
+
     QuestionService questionService;
-    public QuestionController (QuestionService questionService){
-        this.questionService=questionService;
+
+    public QuestionController(QuestionService questionService, Environment environment) {
+        this.questionService = questionService;
+        this.environment = environment;
     }
+
     @RequestMapping("allQuestions")
-    public List<Question> getQuestions(){
+    public List<Question> getQuestions() {
         return questionService.getQuestions();
     }
 
     @GetMapping("category/{category}")
-    public ResponseEntity<List<Question>> getQuestionsByCategory(@PathVariable String category){
+    public ResponseEntity<List<Question>> getQuestionsByCategory(@PathVariable String category) {
         return questionService.getQuestionsByCategory(category);
     }
 
     @PostMapping("add")
-    public ResponseEntity<String> addQuestion(@RequestBody Question question){
-        return  questionService.addQuestion(question);
+    public ResponseEntity<String> addQuestion(@RequestBody Question question) {
+        return questionService.addQuestion(question);
     }
 
     @GetMapping("generate")
     public ResponseEntity<List<Integer>> getQuestionsForQuiz
-            (@RequestParam String categoryName, @RequestParam Integer numQuestions ){
+            (@RequestParam String categoryName, @RequestParam Integer numQuestions) {
         return questionService.getQuestionsForQuiz(categoryName, numQuestions);
     }
 
     @PostMapping("getQuestions")
-    public ResponseEntity<List<QuestionWrapper>> getQuestionsFromId(@RequestParam List<Integer> questionIds){
-    return questionService.getQuestionsFromId(questionIds);
+    public ResponseEntity<List<QuestionWrapper>> getQuestionsFromId(@RequestParam List<Integer> questionIds) {
+        System.out.println(environment.getProperty("local.server.port"));
+        return questionService.getQuestionsFromId(questionIds);
     }
 
     @PostMapping("getScore")
-    public ResponseEntity<Integer> getScore(@RequestBody List<Response> responses){
-        return  questionService.getScore(responses);
+    public ResponseEntity<Integer> getScore(@RequestBody List<Response> responses) {
+        return questionService.getScore(responses);
     }
 }
